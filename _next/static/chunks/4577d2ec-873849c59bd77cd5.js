fc471d28d22b6d978eddf88298c23688939427fc
pranj4 <pranj568@gmail.com>
2025-07-20 20:43:02 +0530

    Updates

---
+++ _next/static/chunks/4577d2ec-873849c59bd77cd5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[817],{1121:function(t,n,u){u.d(n,{YiX:function(){return c}});var a=u(5054);function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"},child:[]}]})(t)}}}]);
