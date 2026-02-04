7a647c91652d5153e5e2b140234c28e3f136d6ba
Shachindra <Shachindra@users.noreply.github.com>
2026-02-04 15:35:09 +0000

    deploy: 8a55bbf8801d5c332d92080ccbbb75fa8621145b

---
+++ _next/static/dqmxe-XdUGYTTRXxBeBTP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2f8fd5c91f36788d.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
